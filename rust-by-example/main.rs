05070a107aa3050c708455cfa076cb9e02c428ee
Joseph Doba <josephdoba@gmail.com>
2025-01-05 19:14:08 -0800

    jan 5th

---
+++ rust-by-example/main.rs
@@ -0,0 +1,16 @@
+fn main(){
+// prints:
+    print!("this is \"print!\"");
+    println!("this is \"println!\" ");
+    println!("this is \"println!\" too ");
+    eprint!("this is \"eprint!\"");
+    eprintln!("this is \"eprintln!\"");
+// can string interpolate directly
+    println!("{obj} {subj} {verb}", obj="the", subj="big",verb="dog");
+
+// Rust even checks to make sure the correct number of arguments are used.
+    println!("My name is {0}, {1} {0}", "Bond", "James");
+// FIXME ^ Add the missing argument: "James"
+
+
+}
