ebc380fda44334e6bf48626095aababbc46b3f7d
Joseph Doba <josephdoba@gmail.com>
2025-01-08 23:20:38 -0800

    jan 8th

--- rust-by-example/main.rs
+++ rust-by-example/main.rs
@@ -15,7 +15,19 @@ fn main() {
 
     // Rust even checks to make sure the correct number of arguments are used.
     println!("My name is {0}, {1} {0}", "Bond", "James");
-    // FIXME ^ Add the missing argument: "James"
+    // FIXME ^ Add the missing argument: "James" -- fixed
 
-    
+    // Primitives
+
+    let num1_float: f64 = 4.1; // regular notation
+    let num2_int = 5i32; //Suffix notation
+
+    //if undefined, defaults will be used: f64 and i32
+    let def_float = 3.2;
+    let def_int = 9;
+
+    println!(
+        "num1 = {}, num2 = {}, num3 = {}, num4 = {}",
+        num1_float, num2_int, def_float, def_int
+    );
 }
