9e05076dcad8fbe5b35a50379cab1f26b56d5f5a
Joseph Doba <josephdoba@gmail.com>
2025-02-28 21:46:49 -0800

    ownership tutorial

--- ownership/ownership-example/src/main.rs
+++ ownership/ownership-example/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("This is the guide to ownership in Rust");
+    println!(
+        "therefore, the following is valid ownership since its within the brackets of our main()"
+    );
+
+    let owner1 = "Hello!";
+    let owner2 = "Hiya!";
+
+    println!("{}", owner1);
+    println!("{}", owner2);
 }
