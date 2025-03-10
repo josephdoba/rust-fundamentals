f37d952abc0151113b555916795de45f75e77b65
Joseph Doba <josephdoba@gmail.com>
2025-03-10 16:46:17 -0700

    debug file cleanup

--- ownership/ownership-example/target/debug/deps/ownership_example.d
+++
@@ -1,5 +0,0 @@
-F:\Comp Sci\Rust\rust-practice\ownership\ownership-example\target\debug\deps\ownership_example.exe: src\main.rs
-
-F:\Comp Sci\Rust\rust-practice\ownership\ownership-example\target\debug\deps\ownership_example.d: src\main.rs
-
-src\main.rs:
--- ownership/ownership-example/target/debug/ownership-example.d
+++
@@ -1,1 +0,0 @@
-F:\Comp\ Sci\Rust\rust-practice\ownership\ownership-example\target\debug\ownership-example.exe: F:\Comp\ Sci\Rust\rust-practice\ownership\ownership-example\src\main.rs
