3ef33284901cf0f7463fbf4974d941f9545d53b8
Joseph Doba <josephdoba@gmail.com>
2025-03-10 17:24:29 -0700

    struct tutorial

--- struct_tutorial/src/main.rs
+++ struct_tutorial/src/main.rs
@@ -10,6 +10,7 @@ fn main() {
      */
 
     //struct syntax:
+    #[derive(Debug)]
     struct User {
         active: bool,
         username: String,
@@ -28,6 +29,21 @@ fn main() {
     // dot notation works!
     println!("{}", user1.username);
 
+    //take note of the arrow here, its called Field Init Shorthanding -- since its redundant to write the arguements twice, doing this just assigns those args as the value into that key/value pair.
+    fn build_user(email: String, username: String) -> User { 
+        User {
+            active: true,
+            username,
+            email,
+            sign_in_count: 1,
+        }
+    }
+
+    let user2 = build_user("example@example.com".to_string(), "bob".to_string());
+
+    println!("{:?}", user2); // I don't understand yet why this needs to be in debug mode before I can print out the object, and printed with the :? flag.... ooh I see, its part of Rusts design philosphy of being explicit. Got it! 
+
+
     struct Rectangle {
         width: u32,
         height: u32
@@ -38,6 +54,8 @@ fn main() {
     }
 
     println!("{}",area(&Rectangle { width: (30), height: (25) }));
-
     println!("{}", area(&Rectangle{ width: (10), height: (12) }));
+
+
+
 }
