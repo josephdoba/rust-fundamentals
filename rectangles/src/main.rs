f4ed569575847b009d9c6d1018d8f3393bb97a0f
Joseph Doba <josephdoba@gmail.com>
2025-03-13 20:12:40 -0700

    structs tutorial

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
+    let width1 = 30;
+    let height1 = 50;
     
 
-    
+    println!("The area of this rectangle is {} px", area(width1,height1))
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
 }
